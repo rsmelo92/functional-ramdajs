af1054d3957f39c62bf392be07fe9492c73266e0
Rafael Melo <rsmelo_@hotmail.com>
2021-02-27 16:45:51 -0300

    Add entity working 100%

--- src/my-db/index.js
+++ src/my-db/index.js
@@ -4,15 +4,24 @@ import addEntity from './modules/addEntity';
 
 const DB = createDB();
 const withNewTable = addTable(DB, 'table-2');
+
 const newEntity = {
   birthday: '14/02/1992',
   cpf: '756.645.432-23',
   email: 'aldebrando.antunes@email.com',
   name: 'Aldebrando Antunes Saraiva',
 };
-
 const withNewEntity = addEntity(withNewTable, 'table-2', 'entity-0', newEntity);
 
-console.log(withNewEntity);
+console.log({ withNewEntity });
+
+const withAnotherEntity = addEntity(withNewEntity, 'table-2', 'entity-1', {
+  birthday: '24/04/1994',
+  cpf: '987.674.542-43',
+  email: 'carla-brandao@email.com',
+  name: 'Carla Brandao Soares',
+});
+
+console.log(withAnotherEntity);
 
 // export { createDB };
--- src/my-db/modules/addEntity.js
+++ src/my-db/modules/addEntity.js
@@ -7,20 +7,17 @@ function addEntity(db, tableId, entityId, entityBody) {
     objOf('entityId', entityId),
     entityBody,
   );
-
   const tables = prop('tables', db);
   const currentTable = find(propEq('tableId', tableId), tables);
-
   const tableTransformations = objOf(
     'entities',
     append(newEntity),
   );
   const transformedTable = evolve(tableTransformations, currentTable);
-
-  const index = indexOf(currentTable, tables);
+  const tableIndex = indexOf(currentTable, tables);
   const newDBTransformations = objOf(
     'tables',
-    update(index, transformedTable),
+    update(tableIndex, transformedTable),
   );
   const transformedDB = evolve(newDBTransformations, db);
 
