bc8beea3cfa65c29c4ab3c0243809331737e8f43
Rafael Melo <rsmelo_@hotmail.com>
2021-02-27 20:16:09 -0300

    Remove comments

--- src/my-db/jest.config.js
+++ src/my-db/jest.config.js
@@ -5,15 +5,11 @@ module.exports = {
     "js",
   ],
   notify: true,
-  // A preset that is used as a base for Jest's configuration
   preset:  "rollup-jest",
   resetMocks: true,
   testEnvironment: "node",
   testPathIgnorePatterns: [
     "/node_modules/"
   ],
-  // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
   timers: "fake",
-  // Whether to use watchman for file crawling
-  // watchman: true,
 };
