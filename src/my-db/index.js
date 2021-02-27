bc42b782e5d785b8b24db2973c92244224875932
Rafael Melo <rsmelo_@hotmail.com>
2021-02-27 15:52:54 -0300

    Refactor createDB and addTable

--- src/my-db/index.js
+++ src/my-db/index.js
@@ -3,18 +3,18 @@ import addTable from './modules/addTable';
 import addEntity from './modules/addEntity';
 
 const DB = createDB();
+console.log(DB);
 const withNewTAble = addTable(DB, 'table-2');
+console.log(withNewTAble);
+// const newEntity = {
+//   birthday: '14/02/1992',
+//   cpf: '756.645.432-23',
+//   email: 'aldebrando.antunes@email.com',
+//   name: 'Aldebrando Antunes Saraiva',
+// };
 
-const newEntity = {
-  "birthday": "14/02/1992",
-  "cpf": "756.645.432-23",
-  "email": "aldebrando.antunes@email.com",
-  "name": "Aldebrando Antunes Saraiva"
-}
-
-const withNewEntity = addEntity(withNewTAble, 'table-2', 'entity-0', newEntity)
+// const withNewEntity = addEntity(withNewTAble, 'table-2', 'entity-0', newEntity);
 
 // console.log(withNewEntity);
 
 // export { createDB };
-
--- src/my-db/modules/createDB.js
+++ src/my-db/modules/createDB.js
@@ -1,28 +1,7 @@
-import { map, pipe, path, nth, toPairs, remove, append } from 'ramda';
 import { data } from '../data-model';
 
 function createDB() {
-  function formatDB(arrayTable) {
-    const getEntities = pipe(
-      nth(2),
-      nth(1),
-      map(toPairs),
-    );
-  
-    const entities = getEntities(arrayTable);
-  
-    const getNewTable = pipe(
-      remove(2,1),
-      append(['entities', entities])
-    );
-  
-    return getNewTable(arrayTable);
-  }
-
-  const tables = path(['tables'], data);
-  const formattedTables = map(toPairs, tables);
-
-  return map(formatDB, formattedTables);
+  return data;
 }
 
 export default createDB;
