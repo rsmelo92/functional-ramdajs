e013cf749f82a38a8c15a53c62240b2a67572744
Rafael Melo <rsmelo_@hotmail.com>
2021-02-28 17:45:02 -0300

    Working denormalized

--- src/my-db/modules/denormalize/index.js
+++ src/my-db/modules/denormalize/index.js
@@ -1,25 +1,30 @@
 import {
-  is, map,
-  path,
-  filter,
-  unnest,
-  compose,
+  map, path,
+  compose, find, propEq, prop,
+  evolve,
+  objOf, assocPath,
 } from 'ramda';
 
 function denormalize(db) {
-  const getReferences = compose(
-    filter(is(Array)),
-    map(path(['cpf'])),
-    unnest,
-    map(path(['entities'])),
-    path(['tables']),
-  );
+  function query(queryArray) {
+    const queryValueFromReference = compose(
+      prop(queryArray[2]),
+      find(propEq('entityId', queryArray[1])),
+      path(['entities']),
+      find(propEq('tableId', queryArray[0])),
+    );
+    return queryValueFromReference(path(['tables'], db));
+  }
+  const transformations = objOf('cpf', query);
 
-  const allReferences = getReferences(db);
+  const accountEntities = path(['tables', 1, 'entities'], db);
+  const denormalizedAccounts = map(evolve(transformations), accountEntities);
+  const denormalizedDB = assocPath(['tables', 1, 'entities'], denormalizedAccounts, db);
 
-  console.log(allReferences);
+  // eslint-disable-next-line no-console
+  console.dir(denormalizedDB, { depth: null });
 
-  return db;
+  return denormalizedDB;
 }
 
 export default denormalize;
