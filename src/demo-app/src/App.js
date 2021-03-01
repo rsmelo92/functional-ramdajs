1a57630fa20811430d56c2ae46bc5efc83f2bdd7
Rafael Melo <rsmelo_@hotmail.com>
2021-02-28 23:32:04 -0300

    Start project and add card

---
+++ src/demo-app/src/App.js
@@ -0,0 +1,16 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-main">
+        <Card>
+          <p>Oi</p>
+        </Card>
+      </main>
+    </div>
+  );
+}
+
+export default App;
