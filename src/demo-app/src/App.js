0300f5e35d7d416ef136a3f6d6f2e9a1a5b32f46
Rafael Melo <rsmelo_@hotmail.com>
2021-03-01 00:33:31 -0300

    Add almost all css

--- src/demo-app/src/App.js
+++ src/demo-app/src/App.js
@@ -1,12 +1,15 @@
 import Card from './components/Card';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <main className="App-main">
-        <Card>
-          <p>Oi</p>
+        <Card className="App-main-card" depth={2}>
+          <h1 className="App-title">My Database</h1>
+          <Table />
+          <Table />
         </Card>
       </main>
     </div>
--- src/demo-app/src/components/Card/index.js
+++ src/demo-app/src/components/Card/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.css'
 
-function Card({ children }) {
+function Card({ children, className, depth = 1 }) {
   return (
-    <div className="card">
+    <div className={`card ${className} depth-${depth}`}>
       {children}
     </div>
   );
